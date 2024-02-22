abf52b8f4fe871f8e2642ec2a563d6b46a1c359d
Jdsatashi <vdt1073@gmail.com>
2024-02-22 09:42:59 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
 import { useState, useEffect } from 'react';
-import CreateBlog from './Components/CreateBlog';
-import CardBlog from './Components/CardBlog';
 
 function App() {
-  // Get storage
-  const storageBlogs = JSON.parse(localStorage.getItem('blogs'))
-  // State
-  const [blogs, setBlogs] = useState(storageBlogs ?? []);
-  // Effect funtions
-  useEffect(() => {
-    localStorage.setItem('blogs', JSON.stringify(blogs));
-  }, [blogs]);
-
-  // Functions
-  function addBlog(data){
-    setBlogs((blogs) => [...blogs, data])
-  }
   return (
     <>
       <div>
-        <h3 className='py-4 text-2xl font-semibold bg-slate-100 text-slate-800'>
-          <span className='mx-1 text-3xl font-bold text-emerald-500'>
-            JDsatashi
-          </span>
-          Simple Blog App
-        </h3>
+        <h1>Hello world</h1>
       </div>
-      <CreateBlog blogs={blogs} createBlog={addBlog}/>
-      {blogs.map((blog, idx) => (
-        <CardBlog key={idx} blog={blog} />
-      ))}
     </>
   );
 }
